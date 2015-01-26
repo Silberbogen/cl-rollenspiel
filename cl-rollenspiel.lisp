d110fb900a9e731c8775ed13898ca788244dd7c1
Sascha Biermanns <skkd.h4k1n9@yahoo.de>
2015-01-26 17:25:18 +0100

    Funktion Würfelpool hinzugefügt

--- cl-rollenspiel.lisp
+++ cl-rollenspiel.lisp
@@ -23,51 +23,14 @@
 (use-package :cl-hilfsroutinen)
 
 
-(defun w4 (&optional (anzahl 1))
-  "Simuliert Würfe mit einem vierseitigen Würfel"
-  (wurfserie anzahl 4))
-
-
-
-(defun w6 (&optional (anzahl 1))
-  "Simuliert Würfe mit einem sechsseitigen Würfel"
-  (wurfserie anzahl 6))
-
-
-
-(defun w8 (&optional (anzahl 1))
-  "Simuliert Würfe mit einem achtseitigen Würfel"
-  (wurfserie anzahl 8))
-
-
-
-(defun w10 (&optional (anzahl 1))
-  "Simuliert Würfe mit einem zehnseitigen Würfel"
-  (wurfserie anzahl 10))
-
-
-
-(defun w12 (&optional (anzahl 1))
-  "Simuliert Würfe mit einem zwölfseitigen Würfel"
-  (wurfserie anzahl 12))
-
-
-
-(defun w20 (&optional (anzahl 1))
-  "Simuliert Würfe mit einem zwanzigseitigen Würfel"
-  (wurfserie anzahl 20))
-
-
-
-(defun w100 (&optional (anzahl 1))
-  "Simuliert Würfe mit einem 100seitigen Würfel"
-  (wurfserie anzahl 100))
-
+;;; #########################
+;;; # Wird NICHT exportiert #
+;;; #########################
 
 
 (defun wurfserie (&optional (anzahl 1) (seiten 6))
-  "Simuliert mehrere Würfe mit einer Anzahl Würfeln"
-  (when (plusp anzahl)
+  "HILFS-FUNKTION: Simuliert mehrere Würfe mit einer Anzahl Würfeln"
+  (if (plusp anzahl)
 	(do ((i 0 (1+ i))
 		 (summe 0)
          geworfene-augen)
@@ -75,4 +38,77 @@
 		 (values summe geworfene-augen))
       (let ((wert (würfelwurf seiten)))
         (incf summe wert)
-        (push wert geworfene-augen)))))
+        (push wert geworfene-augen)))
+    (error "~&Der Aufruf erfolgt mittels (WURFSERIE ANZAHL) oder (WURFSERIE ANZAHL SEITEN), wobei ANZAHL und SEITEN positive Integerzahlen darstellen müssen.")))
+
+
+;;; ###################
+;;; # Wird EXPORTIERT #
+;;; ###################
+
+
+(defun w4 (&optional (anzahl 1))
+  "FUNKTION: Simuliert ANZAHL Würfe mit einem vierseitigen Würfel"
+  (wurfserie anzahl 4))
+
+
+(defun w6 (&optional (anzahl 1))
+  "FUNKTION: Simuliert ANZAHL Würfe mit einem sechsseitigen Würfel"
+  (wurfserie anzahl 6))
+
+
+(defun w8 (&optional (anzahl 1))
+  "FUNKTION: Simuliert ANZAHL Würfe mit einem achtseitigen Würfel"
+  (wurfserie anzahl 8))
+
+
+(defun w10 (&optional (anzahl 1))
+  "FUNKTION: Simuliert ANZAHL Würfe mit einem zehnseitigen Würfel"
+  (wurfserie anzahl 10))
+
+
+(defun w12 (&optional (anzahl 1))
+  "FUNKTION: Simuliert ANZAHL Würfe mit einem zwölfseitigen Würfel"
+  (wurfserie anzahl 12))
+
+
+(defun w20 (&optional (anzahl 1))
+  "FUNKTION: Simuliert ANZAHL Würfe mit einem zwanzigseitigen Würfel"
+  (wurfserie anzahl 20))
+
+
+(defun w100 (&optional (anzahl 1))
+  "FUNKTION: Simuliert ANZAHL Würfe mit einem 100seitigen Würfel"
+  (wurfserie anzahl 100))
+
+
+(defun würfelpool (&optional (anzahl 1) (schwellenwert 1))
+  "FUNKTION: Würfelt einen Würfelpool mit ANZAHL sechsseitiger Würfel, wobei ein SCHWELLENWERT an geworfenen Fünfen oder Sechsen als Erfolg gibt.
+Die Funktion liefert folgende Rückgabewerte in der aufgeführten Reihenfolge zurück:
+- Gesammtsumme aller Würfelaugen
+- Eine Liste aller geworfenen Augen
+- boolsche Antwort, ob ein Erfolg vorliegt
+- ANZAHL Nettoerfolge, wenn vorhanden, sonst NIL
+- boolsche Antwort, ob ein Patzer vorliegt
+- boolsche Antwort, ob ein kritischer Patzer vorliegt"
+  (multiple-value-bind (wert würfel) (w6 anzahl)
+    (let ((erfolge 0)
+          (einser 0)
+          erfolg
+          nettoerfolge
+          patzer
+          kritischer-patzer)
+      (dolist (wurf würfel)
+        (when (>= wurf 5)
+          (incf erfolge))
+        (when (= wurf 1)
+          (incf einser)))
+      (when (>= erfolge schwellenwert)
+        (setf erfolg 't))
+      (setf nettoerfolge (- erfolge schwellenwert))
+      (when (> einser (truncate (/ anzahl 2)))
+        (setf patzer 't))
+      (when (= einser anzahl)
+        (setf kritischer-patzer 't))
+      (values wert würfel erfolg (if (plusp nettoerfolge) nettoerfolge) patzer kritischer-patzer))))
+        
